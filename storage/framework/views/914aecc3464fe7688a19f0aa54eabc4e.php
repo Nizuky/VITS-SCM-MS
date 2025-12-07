7369edc3c2a7e0c3b1095cfa5c93fefc72ad0c65
Nizuki <janarafael.sanandres@gmail.com>
2025-12-08 07:07:21 +0800

    fix: Adjust SVG positioning in search input for better alignment

--- storage/framework/views/914aecc3464fe7688a19f0aa54eabc4e.php
+++ storage/framework/views/914aecc3464fe7688a19f0aa54eabc4e.php
@@ -12,7 +12,7 @@
                 placeholder="Search by name, student ID, or email..." 
                 class="input input-bordered w-full rounded-lg pr-10"
             />
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-3 inset-y-0 my-auto h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
             </svg>
         </div>
