8db3a0c2a2d079cb2fa9e69cd609f2cc5a9c5e35
Nizuki <janarafael.sanandres@gmail.com>
2025-12-09 06:33:20 +0800

    Force clean composer install to fix Livewire cache issue

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Clear composer cache and remove vendor to force fresh install
+rm -rf vendor
+composer clear-cache
 composer install --no-dev --prefer-dist --optimize-autoloader
+
 npm ci
 npm run build
