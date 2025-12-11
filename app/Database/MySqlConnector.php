4e298e8ede00c58d66d6a09cb719cfe305e71f40
Nizuki <janarafael.sanandres@gmail.com>
2025-12-11 20:12:39 +0800

    Fix MySQL connector to completely exclude database from DSN

--- app/Database/MySqlConnector.php
+++ app/Database/MySqlConnector.php
@@ -8,6 +8,49 @@ use PDO;
 class MySqlConnector extends BaseMySqlConnector
 {
     /**
+     * Get the DSN string for a host / port configuration.
+     *
+     * @param  array  $config
+     * @return string
+     */
+    protected function getDsn(array $config)
+    {
+        // Build DSN WITHOUT database name
+        // We'll select it later with USE statement
+        return $this->hasSocket($config)
+            ? $this->getSocketDsn($config)
+            : $this->getHostDsn($config);
+    }
+
+    /**
+     * Get the DSN string for a host / port configuration.
+     *
+     * @param  array  $config
+     * @return string
+     */
+    protected function getHostDsn(array $config)
+    {
+        extract($config, EXTR_SKIP);
+
+        // Don't include dbname in DSN - we'll USE it later
+        return isset($port)
+            ? "mysql:host={$host};port={$port}"
+            : "mysql:host={$host}";
+    }
+
+    /**
+     * Get the DSN string for a socket configuration.
+     *
+     * @param  array  $config
+     * @return string
+     */
+    protected function getSocketDsn(array $config)
+    {
+        // Don't include dbname in DSN
+        return "mysql:unix_socket={$config['unix_socket']}";
+    }
+
+    /**
      * Create a new PDO connection.
      *
      * @param  string  $dsn
@@ -18,16 +61,15 @@ class MySqlConnector extends BaseMySqlConnector
     public function createConnection($dsn, array $config, array $options)
     {
         // Create connection without database selection
-        $dsnWithoutDb = preg_replace('/;dbname=[^;]+/', '', $dsn);
+        $connection = parent::createConnection($dsn, $config, $options);
         
-        $connection = parent::createConnection($dsnWithoutDb, $config, $options);
-        
-        // Manually select database with backticks to handle special characters
+        // Manually select database with backticks to handle special characters like hyphens
         if (isset($config['database']) && $config['database'] !== '') {
-            $database = $config['database'];
+            $database = str_replace('`', '``', $config['database']); // Escape backticks
             $connection->exec("USE `{$database}`");
         }
         
         return $connection;
     }
+}
 }
